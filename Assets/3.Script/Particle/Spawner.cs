94465bf8d241162acf93d42036396b0ab1a60497
juhye1 <wnzk01@gmail.com>
2023-06-30 21:01:15 +0900

    차따르기끝

---
+++ Assets/3.Script/Particle/Spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform trans;
+
+    private void Awake()
+    {
+        StartCoroutine(dd());
+    }
+    private void Start()
+    {
+        
+    }
+
+    private IEnumerator dd()
+    {
+        WaitForSeconds dd = new WaitForSeconds(0.05f);
+
+        for (int i = 0; i < 100; i++)
+
+        {
+            Instantiate(trans, transform.position, Quaternion.identity);
+        yield return dd;
+        }
+    }
+}
--- Assets/3.Script/Particle/bbvkxlzmfgkrltlfj.cs
+++ Assets/3.Script/Particle/bbvkxlzmfgkrltlfj.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class bbvkxlzmfgkrltlfj : MonoBehaviour
 {
-    private ParticleSystem dd;
-    [SerializeField] private ParticleSystem mm;
-
+    Transform t;
+    Rigidbody2D rigid;
     private void Awake()
     {
-        dd = GetComponent<ParticleSystem>();
-    }
-    private void OnParticleCollision(GameObject other)
-    {
-        Debug.Log("´ê¾ÒÀ½");
-        mm.Play();
+        t = GetComponent<Transform>();
+        rigid = GetComponent<Rigidbody2D>();
         
+    }
+    private void Start()
+    {
+        rigid.AddForce(Vector2.left*0.8f, ForceMode2D.Impulse);
+/*        Vector3 dd = new Vector3(-0.096f, 0.916f, 0);
+        Sequence sequence;
+        sequence = DOTween.Sequence();
+        sequence.Append(t.DOJump(dd, 0.1f, 1, 1f));*/
+
     }
 }
