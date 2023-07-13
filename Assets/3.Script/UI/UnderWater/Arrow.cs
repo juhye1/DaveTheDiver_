349e303e30abe8018dbfa05fbf8171000573a07d
juhye1 <wnzk01@gmail.com>
2023-07-13 13:24:05 +0900

    각도

--- Assets/3.Script/UI/UnderWater/Arrow.cs
+++ Assets/3.Script/UI/UnderWater/Arrow.cs
@@ -30,15 +30,34 @@ public class Arrow : MonoBehaviour
             playerScreenPosition = mainCamera.WorldToScreenPoint(daveArm.position);
             Vector2 offset = player.MousePosition - playerScreenPosition;
             float dir = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-            dir = Mathf.Clamp(dir, -20, 20);
+            //Debug.Log(dir);
+
+            //dir = (dir < -130) ? dir += 130 : dir;
+            //Debug.Log(dir);
+            //dir = Mathf.Clamp(dir, -20, 20);
 
             if (player.MousePosition.x > 700)
             {
-                arrow.localRotation = Quaternion.Euler(0, 0, dir);
+                //무조건 0~360값으로 바뀜
+                //  -14도는 345도?
+                //arrow.localRotation = Quaternion.Euler(0,0,dir);
+
+                float right = Mathf.Clamp(dir, -20, 20);
+                arrow.localEulerAngles = new Vector3(0, 0, right);
+                //Debug.Log(arrow.localEulerAngles);
             }
             else
             {
-                arrow.localRotation = Quaternion.Euler(0, 180, dir);
+                Vector3 left = Quaternion.Euler(0, 180, dir).eulerAngles;
+                left.z = Mathf.Clamp(left.z, 160, 200);
+                arrow.localEulerAngles = left;
+/*                Vector3 angle = new Vector3(0, 0, dir);
+                angle.z = (angle.z > 180) ? angle.z - 360 : angle.z;
+                angle.z = Mathf.Clamp(angle.z, -20, 20);
+                arrow.localRotation = Quaternion.Euler(angle);*/
+
+                //Debug.Log(arrow.localRotation.z);
+
             }
 
         }
